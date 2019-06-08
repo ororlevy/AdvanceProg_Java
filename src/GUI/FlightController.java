129c1af4c72b17a2aa7523d65d86c1826cb59876
ororlevy <45944864+ororlevy@users.noreply.github.com>
2019-06-08 13:03:07 +0300

    lasted

--- src/GUI/FlightController.java
+++ src/GUI/FlightController.java
@@ -13,11 +13,16 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import javafx.geometry.Point3D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
@@ -53,9 +58,15 @@ public class FlightController implements Initializable {
     private Canvas map;
     @FXML
     private RadioButton manual;
+    @FXML
+    private Circle border;
+    @FXML
+    private Circle Joystick;
     private ClientSim cls=new ClientSim();
     private Interpter interpter=new Interpter();
-
+    private static int Who=-1;
+    double orgSceneX, orgSceneY;
+    double orgTranslateX, orgTranslateY;
 
 
     public void LoadDate() {
@@ -131,20 +142,43 @@ public class FlightController implements Initializable {
             root = fxmlLoader.load();
             stage.setTitle("Popup");
             stage.setScene(new Scene(root));
-            if(!stage.isShowing())
+            if(!stage.isShowing()) {
                 stage.show();
+                this.Who=0;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+    public void Calc(){
+        Parent root = null;
+        try {
+            FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("Popup.fxml"));
+            root = fxmlLoader.load();
+            stage.setTitle("Popup");
+            stage.setScene(new Scene(root));
+            if(!stage.isShowing()) {
+                this.Who=1;
+                stage.show();
 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void Submit(){
-        cls.Connect(ip.getText(),Integer.parseInt(port.getText()));
-        ip.clear();
-        port.clear();
-        Stage stage = (Stage) submit.getScene().getWindow();
-        stage.close();
+        if(this.Who==0) {
+            cls.Connect(ip.getText(), Integer.parseInt(port.getText()));
+            ip.clear();
+            port.clear();
+            Stage stage = (Stage) submit.getScene().getWindow();
+            stage.close();
+        }
+        if(this.Who==1)
+        {
+
+        }
     }
 
     public void AutoPilot(){
@@ -173,6 +207,65 @@ public class FlightController implements Initializable {
         }
     }
 
+    EventHandler<MouseEvent> circleOnMousePressedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    orgSceneX = t.getSceneX();
+                    orgSceneY = t.getSceneY();
+                    orgTranslateX = ((Circle)(t.getSource())).getTranslateX();
+                    orgTranslateY = ((Circle)(t.getSource())).getTranslateY();
+                }
+            };
+
+    EventHandler<MouseEvent> circleOnMouseDraggedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    double offsetX = t.getSceneX() - orgSceneX;
+                    double offsetY = t.getSceneY() - orgSceneY;
+                    double newTranslateX = orgTranslateX + offsetX;
+                    double newTranslateY = orgTranslateY + offsetY;
+                    if(isInCircle(newTranslateX,newTranslateY)) {
+                        ((Circle) (t.getSource())).setTranslateX(newTranslateX);
+                        ((Circle) (t.getSource())).setTranslateY(newTranslateY);
+                        String[] data={
+                                "set /controls/flight/aileron "+nirmulX(newTranslateX),
+                                "set /controls/flight/elevator "+nirmulY(newTranslateY),
+                        };
+                        cls.Send(data);
+                    }
+                }
+            };
+    private double nirmulX(double num){
+        double max=(border.getRadius()-Joystick.getRadius())+border.getCenterX();
+        double min=border.getCenterX()-(border.getRadius()-Joystick.getRadius());
+        double new_max=1;
+        double new_min=-1;
+        return (((num-min)/(max-min)*(new_max-new_min)+new_min));
+    }
+    private double nirmulY(double num){
+        double min=(border.getRadius()-Joystick.getRadius())+border.getCenterY();
+        double max=border.getCenterY()-(border.getRadius()-Joystick.getRadius());
+        double new_max=1;
+        double new_min=-1;
+        return (((num-min)/(max-min)*(new_max-new_min)+new_min));
+    }
+    private  boolean isInCircle(double x,double y){
+        return (Math.pow((x-border.getCenterX()),2)+Math.pow((y-border.getCenterY()),2))<=Math.pow(border.getRadius()-Joystick.getRadius(),2);
+    }
+    EventHandler<MouseEvent> circleOnMouseReleasedEventHandler =
+            new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent t) {
+
+                    ((Circle)(t.getSource())).setTranslateX(orgTranslateX);
+                    ((Circle)(t.getSource())).setTranslateY(orgTranslateY);
+                }
+            };
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         if(location.getPath().equals("/C:/Users/ororl/GitHub/PTM1/production/GitHub/GUI/Flight.fxml")) {
@@ -184,6 +277,12 @@ public class FlightController implements Initializable {
                 String[] data={"set /controls/flight/rudder "+newValue.doubleValue()};
                 cls.Send(data);
             });
+            Joystick.setOnMousePressed(circleOnMousePressedEventHandler);
+            Joystick.setOnMouseDragged(circleOnMouseDraggedEventHandler);
+            Joystick.setOnMouseReleased(circleOnMouseReleasedEventHandler);
         }
     }
+
+
+
 }
